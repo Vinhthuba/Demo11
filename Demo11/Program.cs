ccbc57dc31742cce2b81ea931a2099f4a1b99911
pc <pc@DESKTOP-C38JTHI>
2022-07-18 13:02:51 -0700

    Add project files.

---
+++ Demo11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace BookStore
+{
+    class Books
+    {
+        static void Main(string[] args)              
+        {
+            string[] bookTitle = new string[6];
+
+            for (int i = 0; i < bookTitle.Length; i++)
+            {
+                Console.Write("Enter the title the book : ");
+                bookTitle[i] = Console.ReadLine();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("List of books : ");
+            foreach (string title in bookTitle)
+            {
+                Console.WriteLine(title);
+            }
+            bookTitle[3] = "Planet of the Apes";
+            Console.WriteLine("NewLy changed title of book 4 :" + bookTitle[3]);
+        }
+    }
+}
